dfbcedb76a94aa6f8e6969296782f7bcf7b3afb7
shinyay <shinya.com@gmail.com>
2020-10-20 23:38:44 +0900

    Add default argument for constructor

--- src/main/kotlin/com/google/shinyay/entity/Customer.kt
+++ src/main/kotlin/com/google/shinyay/entity/Customer.kt
@@ -1,5 +1,5 @@
 package com.google.shinyay.entity
 
-data class Customer (val id: String,
-                     val firstName: String,
-                     val lastName: String)
+data class Customer (val id: String? = null,
+                     val firstName: String? = null,
+                     val lastName: String? = null)
