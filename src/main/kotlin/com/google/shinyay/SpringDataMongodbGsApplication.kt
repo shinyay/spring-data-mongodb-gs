a2f3738181af2f86ddaa89bdc463b1f3fba71d44
shinyay <shinya.com@gmail.com>
2020-10-21 23:01:08 +0900

    Add insert data

--- src/main/kotlin/com/google/shinyay/SpringDataMongodbGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringDataMongodbGsApplication.kt
@@ -1,14 +1,19 @@
 package com.google.shinyay
 
+import com.google.shinyay.entity.Customer
 import com.google.shinyay.repository.CustomerRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
 class SpringDataMongodbGsApplication(val repository: CustomerRepository) : CommandLineRunner {
 	override fun run(vararg args: String?) {
 		repository.deleteAll()
+
+		repository.save(Customer("Alice", "Smith"))
+		repository.save(Customer("Bob", "Smith"))
 	}
 }
 
