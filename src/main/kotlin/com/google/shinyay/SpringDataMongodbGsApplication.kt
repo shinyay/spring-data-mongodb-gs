b91aeb98a1a6ca3fea1614461446356d7d8ce6f8
shinyay <shinya.com@gmail.com>
2020-10-21 22:56:17 +0900

    Add CommandLineRunner interface

--- src/main/kotlin/com/google/shinyay/SpringDataMongodbGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringDataMongodbGsApplication.kt
@@ -1,10 +1,15 @@
 package com.google.shinyay
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringDataMongodbGsApplication
+class SpringDataMongodbGsApplication : CommandLineRunner {
+	override fun run(vararg args: String?) {
+		TODO("Not yet implemented")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringDataMongodbGsApplication>(*args)
